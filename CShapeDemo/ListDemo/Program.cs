ddc8ef64d2d346eab7feeea742c498f889f11067
guoliang wang <tswanggl@hotmail.com>
2021-11-14 17:59:49 +0800

    List operation test

--- CShapeDemo/ListDemo/Program.cs
+++ CShapeDemo/ListDemo/Program.cs
@@ -12,10 +12,47 @@ Console.WriteLine(array is IEnumerable<int>);
 // NOTE(guoliang) Method 3
 List<int> list = new List<int>() { 100, 200, 300, 400 };
 Console.WriteLine($"{list.Count}/{list.Capacity}");
-
-
-
 Console.WriteLine(String.Join(",", list));
+
+List<int> list2 = new List<int> { 10, 20, 30, 40 };
+list2.AddRange(list);
+
+list2.Clear();
+
+Console.WriteLine(String.Join(",", list2));
+
+void test_remove()
+{
+    Console.WriteLine("Test remove ===>");
+    List<int> list = new List<int>() { 100, 200, 300, 400 };
+    list.AddRange(list);
+    list.AddRange(list);
+
+    Console.WriteLine(String.Join(",", list));
+
+    list.RemoveAt(0);
+    Console.WriteLine(String.Join(",", list));
+
+    list.RemoveAt(list.Count - 1 );
+    Console.WriteLine(String.Join(",", list));
+
+    list.RemoveRange(0, 1);
+    Console.WriteLine(String.Join(",", list));
+
+    list.Remove(300);
+    Console.WriteLine(String.Join(",", list));
+
+    list.RemoveAll(e => e == 400);
+    Console.WriteLine(String.Join(",", list));
+}
+
+test_remove();
+
+
+
+
+
+
 
 
 
