4b99e9713ae2e982ca5bf94761f73b783fececeb
guoliang wang <tswanggl@hotmail.com>
2021-11-20 18:44:48 +0800

    list init test

--- CShapeDemo/ListDemo/Program.cs
+++ CShapeDemo/ListDemo/Program.cs
@@ -1,25 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // NOTE (guoliang) Method 1
-int[] array = new int[] { 100, 200, 300, 400 };
-Console.WriteLine(array is IEnumerable<int>);
+void test_init_with_array()
+{
+    Console.WriteLine("Test init with array\n");
+    int[] array = new int[] { 100, 200, 300, 400 };
 
-//List<int> list = new List<int>(array);
+    Console.WriteLine(array is IEnumerable<int>);
+    List<int> list = new List<int>(array);
+    Console.WriteLine(String.Join(",", list));
+}
 
-// NOTE(guoliang) Method 2
-//List<int> list = new List<int>(50);
 
-// NOTE(guoliang) Method 3
-List<int> list = new List<int>() { 100, 200, 300, 400 };
-Console.WriteLine($"{list.Count}/{list.Capacity}");
-Console.WriteLine(String.Join(",", list));
+// NOTE (guoliang) Method 2
+void test_init_with_size()
+{
+    Console.WriteLine("Test init with size?\n");
+    List<int> list = new List<int>(50);
 
-List<int> list2 = new List<int> { 10, 20, 30, 40 };
-list2.AddRange(list);
+    Console.WriteLine($"{list.Count}");
+    Console.WriteLine(String.Join(",", list));
+}
 
-list2.Clear();
+void test_dyn()
+{
+    Console.WriteLine("Test the size and capacity of the list\n");
+    List<int> list = new List<int>();
+    for(int i = 1; i <= 100; i++)
+    {
+        list.Add(i);
+        Console.WriteLine($"{list.Count}/{list.Capacity}");
+    }
+    Console.WriteLine(String.Join(",", list));
+}
 
-Console.WriteLine(String.Join(",", list2));
+
+// NOTE (guoliang) Method 3
+void test_AddRange()
+{
+    List<int> list = new List<int>() { 100, 200, 300, 400 };
+    Console.WriteLine($"{list.Count}/{list.Capacity}");
+    Console.WriteLine(String.Join(",", list));
+
+    Console.WriteLine("After AddRange\n");
+    List<int> list2 = new List<int> { 10, 20, 30, 40 };
+    list2.AddRange(list);
+    Console.WriteLine(String.Join(",", list2));
+}
+
+void test_InsertRange()
+{
+    Console.WriteLine("Test InsertRange:\n");
+    List<int> list1 = new List<int>() { 100, 200, 300, 400 };
+    List<int> list2 = new List<int> { 10, 20, 30, 40 };
+
+    list2.InsertRange(list2.Count, list1);
+    Console.WriteLine(String.Join(",", list2));
+}
 
 void test_remove()
 {
@@ -46,6 +83,11 @@ void test_remove()
     Console.WriteLine(String.Join(",", list));
 }
 
+test_dyn();
+test_init_with_array();
+test_init_with_size();
+test_AddRange();
+test_InsertRange();
 test_remove();
 
 
---
+++ CShapeDemo/ListDemo/books.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace ListDemo
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
