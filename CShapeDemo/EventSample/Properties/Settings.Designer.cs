99c55852d4c407d71ba579a8ee8ae5b7eaa6c4f5
guoliang <tswanggl@gmail.com>
2022-06-30 18:37:40 +0800

    How to change namespace name in one-time?
    can not change namespace name just in one place (main source file)

--- CShapeDemo/EventSample/Properties/Settings.Designer.cs
+++ CShapeDemo/EventSample/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace EventSample.Properties
+namespace EventSample_timer_App.Properties
 {
 
 
--- CShapeDemo/NugetExample/Program.cs
+++ CShapeDemo/NugetExample/Program.cs
@@ -1,4 +1,17 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace NugetExample
 {
@@ -6,7 +19,8 @@ namespace NugetExample
     {
         static void Main(string[] args)
         {
-
+            System.Windows.Window window = new System.Windows.Window();
+            window.Show();
         }
     }
 }
