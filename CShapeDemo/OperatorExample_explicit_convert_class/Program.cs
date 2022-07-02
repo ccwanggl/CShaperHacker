e0acc3c3e49eed2784ad826ce9268ebf2a9d120f
guoliang <tswanggl@gmail.com>
2022-07-02 22:28:51 +0800

    update

--- CShapeDemo/OperatorExample_explicit_convert_class/Program.cs
+++ CShapeDemo/OperatorExample_explicit_convert_class/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Stone stone = new Stone();
+            stone.Age = 5000;
+
+            Monkey wukongSun = (Monkey)stone;
+            Console.WriteLine(wukongSun.Age);
         }
+    }
+
+    class Stone
+    {
+        public int Age;
+
+        public static explicit operator Monkey(Stone stone)
+        {
+            Monkey monkey = new Monkey();
+            monkey.Age = stone.Age / 500;
+            return monkey;
+        }
+    }
+
+    class Monkey
+    {
+        public int Age;
     }
 }
