976ce611037b72521f1d312e3e1b361641c6dc2b
guoliang <tswanggl@gmail.com>
2022-07-05 09:45:20 +0800

    update format string

---
+++ CShapeDemo/GeneralExample_format_string/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GeneralExample_format_string
+{
+    internal class Program
+    {
+        //note(guoliang): In the complicated case, use the var in the format string much better
+        static void Main(string[] args)
+        {
+            int var1 = 1;
+            int var2 = 2;
+
+            Console.WriteLine($"Two sample integers are {var1} and {var2}");
+            Console.WriteLine($"Three integers are {1}, {0} and {1}", 3, 6); 
+        }
+    }
+}
