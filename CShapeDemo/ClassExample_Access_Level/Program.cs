9667a2a53b14b0371130ad161ed1f2f5a71d36c2
guoliang <tswanggl@gmail.com>
2022-07-09 22:24:32 +0800

    class public internal

---
+++ CShapeDemo/ClassExample_Access_Level/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassExample_Access_Level_Lib.MyNamespace;
+
+
+namespace ClassExample_Access_Level
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1,2));
+        }
+    }
+}
