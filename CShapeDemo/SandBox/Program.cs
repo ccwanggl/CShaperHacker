f2bc1dc45b162e517a31d28c3ba0228f9cabcdf8
guoliang <tswanggl@gmail.com>
2022-08-10 00:05:17 +0800

    ignore .idea dir

--- CShapeDemo/SandBox/Program.cs
+++ CShapeDemo/SandBox/Program.cs
@@ -1,21 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System;
 
-namespace SandBox
-{
-    class SandBox
-    {
-        static void Main(string[] args)
-        {
-            // NOTE(guoliang) "var" the same as to the keyword "auto" in the C++?
-
-            var result = Tools.Calculator.Div(1, 0);
-            var mul = Tools.Calculator.Mul(1, 0);
-
-            Console.WriteLine(result);
-            Console.WriteLine(mul);
-
-        }
-    }
-}
+Console.WriteLine(Tools.Calculator.Add(1, 3));
