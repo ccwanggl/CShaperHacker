e79eafc45bcb9f3e57cc13738ba22ea2335ffb69
guoliang <tswanggl@gmail.com>
2022-12-03 23:18:09 +0800

    update

--- CShapeDemo/GeneralExample_variable_array/Program.cs
+++ CShapeDemo/GeneralExample_variable_array/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] arr = new int[100];
+            arr[0] = 100;
+            Console.WriteLine(arr[0]); 
         }
     }
 }
