b6a092f7f68e8dc00eba7c3e47f4ede0cf1482b4
guoliang <tswanggl@gmail.com>
2022-12-07 16:13:13 +0800

    nullable

---
+++ CShapeDemo/ExpressionExample_nullable/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ExpressionExample_nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x = null;
+
+            var y = x ?? 100;
+            Console.WriteLine(y.GetType().FullName);
+        }
+    }
+}
