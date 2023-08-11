64927daf75fe0a12051b15f0428dee1737b20175
Guoliang WANG <tswanggl@gmail.com>
2023-08-11 11:07:31 +0800

    keyword "Type" example
    keyword "Type"

---
+++ CSharpConcepts/03_CSharpProgrammingType/TypeSample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace TypeSample
+{
+    internal class Program
+    {
+        // NOTE: 反射时应用
+        private static void Main()
+        {
+            Type myType = typeof(Form);
+
+            PropertyInfo[] pInfo = myType.GetProperties();
+            MethodInfo[] mInfo = myType.GetMethods();
+
+            //var pInfo = myType.GetProperties();
+            //var mInfo = myType.GetMethods();
+
+            Console.WriteLine(pInfo.GetType().Name);
+            Console.WriteLine(mInfo.GetType().Name);
+
+            Console.WriteLine("===============================");
+
+            foreach(var m in pInfo)
+            {
+                Console.WriteLine(m.Name);
+            }
+            
+            Console.WriteLine();
+
+            foreach(var m in mInfo)
+            {
+                Console.WriteLine(m.Name);
+            }
+        }
+    }
+}
