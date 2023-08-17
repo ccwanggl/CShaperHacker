5ae65776cfdc5728c1cddf34466be7450ca4c678
Guoliang WANG <tswanggl@gmail.com>
2023-08-17 17:54:26 +0800

    layout stackpanel

--- CSharpFramework/premake5.lua
+++ CSharpFramework/premake5.lua
@@ -2,7 +2,11 @@ group "CSharpFramework/WPF"
 	include "src/WPF/ScottPlot"
 	include "src/WPF/ContentControl"
 	include "src/WPF/LayoutContent"
+group ""
+
+group "CSharpFramework/WPF/Layout"
 	include "src/WPF/LayoutGrid"
+	include "src/WPF/LayoutStackpanel"
 group ""
 
 group "CSharpFramework/WPF/XAML"
