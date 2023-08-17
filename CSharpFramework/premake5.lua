785886dad9de764d8868ffaeeafdfdc359dae920
Guoliang WANG <tswanggl@gmail.com>
2023-08-17 17:37:16 +0800

    update

--- CSharpFramework/premake5.lua
+++ CSharpFramework/premake5.lua
@@ -1,5 +1,8 @@
 group "CSharpFramework/WPF"
 	include "src/WPF/ScottPlot"
+	include "src/WPF/ContentControl"
+	include "src/WPF/LayoutContent"
+	include "src/WPF/LayoutGrid"
 group ""
 
 group "CSharpFramework/WPF/XAML"
