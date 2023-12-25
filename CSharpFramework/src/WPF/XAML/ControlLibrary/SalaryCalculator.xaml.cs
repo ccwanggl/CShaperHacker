daf1f94411fc7b13708d14134c4f750769a382c8
Guoliang WANG <tswanggl@gmail.com>
2023-12-25 10:20:00 +0800

    update

---
+++ CSharpFramework/src/WPF/XAML/ControlLibrary/SalaryCalculator.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlLibrary
+{
+	/// <summary>
+	/// Interaction logic for SalaryCalculator.xaml
+	/// </summary>
+	public partial class SalaryCalculator : UserControl
+	{
+		public SalaryCalculator()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, RoutedEventArgs e)
+		{
+			this.textBox3.Text = this.textBox1.Text + this.textBox2.Text;
+        }
+    }
+}
