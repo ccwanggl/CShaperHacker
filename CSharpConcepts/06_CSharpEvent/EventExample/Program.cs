337c1610b3b6bdd6aab1fa297711cb95946c0c44
Guoliang WANG <tswanggl@gmail.com>
2024-01-04 20:13:53 +0800

    基本事件
    事件拥有者
    事件
    事件订阅者
    事件响应者
    事件处理器

--- CSharpConcepts/06_CSharpEvent/EventExample/Program.cs
+++ CSharpConcepts/06_CSharpEvent/EventExample/Program.cs
@@ -7,11 +7,16 @@ namespace EventExample
     {
         static void Main(string[] args)
         {
+            // NOTE: 事件拥有者
             Timer timer = new Timer();
             timer.Interval = 1000;
 
+            // NOTE 事件响应者
             Boy boy = new Boy();
             Girl girl = new Girl();
+
+            // NOTE: Elapsed 事件
+            // NOTE: += 事件订阅 subscribe
             timer.Elapsed += boy.Action;
             timer.Elapsed += girl.Action;
             timer.Start();
@@ -21,6 +26,7 @@ namespace EventExample
 
     class Boy
     {
+        // NOTE: 事件处理器 event handler
         internal void Action(object sender, ElapsedEventArgs e)
         {
             Console.WriteLine("Jump!"); 
