51e9d0c841e4b198d2e45919fe2473ba57c6540a
guoliang <guoliang.wang@cust.edu.cn>
2024-05-08 07:53:04 +0800

    update

--- premake5.lua
+++ premake5.lua
@@ -1,19 +1,31 @@
 include "./vendor/premake_customization/solution_items.lua"
 workspace "CSharpHacker"
+    architecture "x86_64"
     solution_items
     {
         ".editorconfig"
     }
 
+    configurations
+    {
+        "Debug",
+        "Release",
+    }
+
+    flags
+    {
+        "MultiProcessorCompile"
+    }
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-include "vendor/premake-core"
 -- projects
+
 group "PracticePrjs"
 	include "CSharpHacker/vendor/EmployedSchedulerSystemSource/EmployedSchedulerSystem"
 group ""
+
 include "CSharpConcepts"
 include "CSharpFramework"
+include "CSharpReposTest"
 include "vendor"
-include "Github"
