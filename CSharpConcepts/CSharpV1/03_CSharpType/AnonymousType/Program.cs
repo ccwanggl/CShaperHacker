f32b16a4db4047376803a683f50c1f0634694e2e
Guoliang WANG <tswanggl@gmail.com>
2024-05-17 23:18:14 +0800

    update

--- CSharpConcepts/CSharpV1/03_CSharpType/AnonymousType/Program.cs
+++ CSharpConcepts/CSharpV1/03_CSharpType/AnonymousType/Program.cs
@@ -2,6 +2,11 @@
 
 namespace AnonymousType
 {
+	// NOTE: 使用 Var 关键字的重要条件：
+	// NOTE: 只能用于局部变量，不能用于字段
+	// NOTE: 只能在变量申明中包含初始化时使用
+	// NOTE: 一旦编译器推断出变量的类型,它就是固定且不能更改的
+
     internal class Program
     {
         private static void Main(string[] args)
---
+++ CSharpConcepts/CSharpV1/04_CSharpClass/AbstractMember/premake5.lua
@@ -0,0 +1,28 @@
+project "AbstractMember"
+	kind "ConsoleApp"
+	language "C#"
+	dotnetframework "4.8"
+	csversion "7.0"
+	clr "unsafe"
+
+	targetdir (finaldir .. "/bin/" .. outputdir .. "/%{prj.name}")
+	objdir (finaldir .. "/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"**.cs",
+	}
+
+	links
+	{
+		"System",
+		"Microsoft.CSharp",
+	}
+
+	filter "configurations:Debug"
+		optimize "Off"
+		symbols "Default"
+
+	filter "configurations:Release"
+		optimize "On"
+		symbols "Default"
