521708da0c89b5c04e68db924e61b102cd6584af
guoliang (WORKSTATION) <guoliang.wang@cust.edu.cn>
2024-12-23 09:54:16 +0800

    DependencyObject and DependencyProperty

--- CSharpFramework/src/WPF/Property/HelloProperty/MainWindow.xaml.cs
+++ CSharpFramework/src/WPF/Property/HelloProperty/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace HelloProperty
@@ -11,31 +12,26 @@ namespace HelloProperty
 		public MainWindow()
 		{
 			InitializeComponent();
-		}
-
-		private void Button_Click(object sender, RoutedEventArgs e)
-		{
-			Person person = new Person
-			{
-				Name = Textbox1.Text			// SetValue
-			};
-			Textbox2.Text = person.Name;        // GetValue
+			
+			var person = new Person();
+			person.SetBinding(Person.NameProperty, new Binding("Text") { Source = Textbox1 });
+			Textbox2.SetBinding(TextBox.TextProperty, new Binding("Name") { Source = person });
 		}
 	}
 
-	// ! DependencyProperty must be used in the DependencyObject
+	//! DependencyProperty must be used in the DependencyObject
 	public class Person : DependencyObject
 	{
-		// ! use public static readonly to 
+		//! use public static readonly to 
 		public static readonly DependencyProperty NameProperty;
 
 		static Person()
 		{
 			//! not use "new", use "Register" to create property
 			NameProperty = DependencyProperty.Register(
-				"Name",                     // which CLR property as wrapper
-				typeof(string),             // what type that DependencyProperty will store
-				typeof(Person));            // what the DependencyProperty belong to
+				"Name",                    // which CLR property as wrapper
+				typeof(string),					// what type the DependencyProperty will store
+				typeof(Person));				// what type the DependencyProperty related to
 		}
 
 		public string Name
